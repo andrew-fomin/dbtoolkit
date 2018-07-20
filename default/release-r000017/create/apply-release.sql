2d35def66bbc2900cbd654f8af09cd34c715e37b
andrew-fomin <andrew.fomin@rittmanmead.com>
2018-07-20 14:55:42 +0300

    updated current-release folder

---
+++ default/release-r000017/create/apply-release.sql
@@ -0,0 +1,11 @@
+spool apply.sql.log
+@@test(6).sql
+@@test(7).sql
+@@test(5).sql
+@@test(2).sql
+@@test(3).sql
+@@test(4).sql
+@@test(8).sql
+@@test(1).sql
+spool off
+quit
---
+++ default/release-r000017/revert/revert-release.sql
@@ -0,0 +1,4 @@
+spool revert.sql.log
+@@rtest.sql
+spool revert.sql.log
+quit
