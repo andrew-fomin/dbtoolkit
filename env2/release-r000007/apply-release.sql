3c93ea4e68a1a2bf1987f5f0aa0e24b60e9ba419
andrew-fomin <andrew.fomin@rittmanmead.com>
2018-07-31 12:06:13 +0300

    updated current-release folder

---
+++ env2/release-r000007/apply-release.sql
@@ -0,0 +1,10 @@
+set termout off
+spool apply.log
+prompt Starting execution of ../D:\RM\dbtoolkit\env2\current-release\create\Tables\table.sql
+@@create/../D:\RM\dbtoolkit\env2\current-release\create\Tables\table.sql
+prompt Finished execution of ../D:\RM\dbtoolkit\env2\current-release\create\Tables\table.sql
+prompt Starting execution of ../D:\RM\dbtoolkit\env2\current-release\create\topl.sql
+@@create/../D:\RM\dbtoolkit\env2\current-release\create\topl.sql
+prompt Finished execution of ../D:\RM\dbtoolkit\env2\current-release\create\topl.sql
+spool off
+quit
