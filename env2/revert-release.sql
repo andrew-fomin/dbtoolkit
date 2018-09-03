bb5453bab7b0d25d2b80cf62ed568f0cbe841beb
andrew-fomin <andrew.fomin@rittmanmead.com>
2018-09-03 18:09:20 +0300

    updated current-release folder

---
+++ env2/revert-release.sql
@@ -0,0 +1,16 @@
+set termout on
+spool revert.log
+prompt Starting execution of revert (1).sql
+@@"revert/revert (1).sql"
+prompt Finished execution of revert (1).sql
+prompt Starting execution of revert (2).sql
+@@"revert/revert (2).sql"
+prompt Finished execution of revert (2).sql
+prompt Starting execution of revert (3).sql
+@@"revert/revert (3).sql"
+prompt Finished execution of revert (3).sql
+prompt Starting execution of revert (4).sql
+@@"revert/revert (4).sql"
+prompt Finished execution of revert (4).sql
+spool revert.sql.log
+quit
