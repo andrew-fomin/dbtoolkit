2e2a2c020e2fc15f341747f21dd73a2687925880
andrew-fomin <andrew.fomin@rittmanmead.com>
2018-09-04 19:01:12 +0300

    test07

--- env2/Releases/apply-release-test07.sql
+++ env2/Releases/apply-release-test07.sql
@@ -1,7 +1,7 @@
 set termout on
 spool apply.log
-prompt Starting execution of Scripts/tests (1).sql
-@@"create/Scripts/tests (1).sql"
-prompt Finished execution of Scripts/tests (1).sql
+prompt Starting execution of Scripts/tests (2).sql
+@@"create/Scripts/tests (2).sql"
+prompt Finished execution of Scripts/tests (2).sql
 spool off
 quit
