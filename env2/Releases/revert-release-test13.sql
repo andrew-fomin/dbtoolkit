e2f61b4cb06413b57b70d4ecd97aff3f8e897853
andrew-fomin <andrew.fomin@rittmanmead.com>
2018-09-04 17:31:19 +0300

    test13

---
+++ env2/Releases/revert-release-test13.sql
@@ -0,0 +1,7 @@
+set termout on
+spool revert.log
+prompt Starting execution of Scripts/test (4).sql
+@@"revert/Scripts/test (4).sql"
+prompt Finished execution of Scripts/test (4).sql
+spool revert.sql.log
+quit
