99890a68d60d3e8fefae587f496eb347574500a2
andrew-fomin <andrew.fomin@rittmanmead.com>
2018-09-04 18:22:13 +0300

    Initial commit

--- env2/Releases/apply-release-test14.sql
+++
@@ -1,7 +0,0 @@
-set termout on
-spool apply.log
-prompt Starting execution of Scripts/test (1).sql
-@@"create/Scripts/test (1).sql"
-prompt Finished execution of Scripts/test (1).sql
-spool off
-quit
--- env2/Releases/revert-release-test2.sql
+++
@@ -1,7 +0,0 @@
-set termout on
-spool revert.log
-prompt Starting execution of Scripts/test (4).sql
-@@"revert/Scripts/test (4).sql"
-prompt Finished execution of Scripts/test (4).sql
-spool revert.sql.log
-quit
