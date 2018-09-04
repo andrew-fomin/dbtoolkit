f36a8efa3905cc76788f7f7fc50b96bd30419fd6
andrew-fomin <andrew.fomin@rittmanmead.com>
2018-09-04 17:08:51 +0300

    test6

---
+++ env2/Releases/apply-release-test6.sql
@@ -0,0 +1,7 @@
+set termout on
+spool apply.log
+prompt Starting execution of Scripts/test (1).sql
+@@"create/Scripts/test (1).sql"
+prompt Finished execution of Scripts/test (1).sql
+spool off
+quit
