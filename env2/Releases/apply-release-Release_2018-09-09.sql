5c6a858a0dee18f5a4e9c56e85997ecb3afb62ad
andrew-fomin <andrew.fomin@rittmanmead.com>
2018-09-09 16:48:46 +0300

    Release_2018-09-09

---
+++ env2/Releases/apply-release-Release_2018-09-09.sql
@@ -0,0 +1,10 @@
+set termout on
+spool apply.log
+prompt Starting execution of Create/1.txt
+@@"Create/1.txt"
+prompt Finished execution of Create/1.txt
+prompt Starting execution of PLSQL/1.txt
+@@"PLSQL/1.txt"
+prompt Finished execution of PLSQL/1.txt
+spool off
+quit
