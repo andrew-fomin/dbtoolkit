735de3c9cf7afe50ecbab6b1e2e3763ad9548d9c
andrew-fomin <andrew.fomin@rittmanmead.com>
2018-09-12 14:47:19 +0300

    r1447

---
+++ env2/Releases/apply-release-r1447.sql
@@ -0,0 +1,13 @@
+set termout on
+spool apply.log
+prompt Starting execution of Create/1.txt
+@@"Create/1.txt"
+prompt Finished execution of Create/1.txt
+prompt Starting execution of PLSQL/1.txt
+@@"PLSQL/1.txt"
+prompt Finished execution of PLSQL/1.txt
+prompt Starting execution of Table/1.txt
+@@"Table/1.txt"
+prompt Finished execution of Table/1.txt
+spool off
+quit
---
+++ env2/Releases/revert-release-r1447.sql
@@ -0,0 +1,13 @@
+set termout on
+spool revert.log
+prompt Starting execution of Create/2.txt
+@@"Create/2.txt"
+prompt Finished execution of Create/2.txt
+prompt Starting execution of PLSQL/2.txt
+@@"PLSQL/2.txt"
+prompt Finished execution of PLSQL/2.txt
+prompt Starting execution of Table/2.txt
+@@"Table/2.txt"
+prompt Finished execution of Table/2.txt
+spool revert.sql.log
+quit
